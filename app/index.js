b1c2a0d0416d6c552805fb55be465846973b3105
Kevin Garwood <kevin.garwood@digital.cabinet-office.gov.uk>
2019-02-13 09:30:18 +0000

    Support pasting into the text area using the current selection range
    Pasted text should be inserted at the current cursor position in the text
    area.  If there is any text currently selected in the box, the pasted text
    should replace it.
    
    The routine getInputSelection() in index.js has been adapted from code in
    the MIT licenced repository written by Tim Down and available at:
    https://github.com/timdown/rangyinputs.
    
    Variables in index.js and harness_demo.html have been renamed to be more
    readable and to fit better with convention.

--- app/index.js
+++ app/index.js
@@ -30,3 +30,69 @@ function remove_unsupported_html_features(html) {
                        allowedAttributes: white_listed_attributes,
                        transformTags: tag_transformations})
 }
+
+
+function getInputSelection(textAreaElement) {
+  // Adapted from code at
+  // https://ourcodeworld.com/articles/read/282/how-to-get-the-current-cursor-position-and-selection-within-a-text-input-or-textarea-in-javascript
+  // which is available under MIT
+  var start = 0;
+  var end = 0;
+  var normalizedValue;
+  var range;
+  var textInputRange;
+  var length;
+  var endRange;
+
+  if (typeof textAreaElement.selectionStart == "number" &&
+      typeof textAreaElement.selectionEnd == "number") {
+    start = textAreaElement.selectionStart;
+    end = textAreaElement.selectionEnd;
+  } else {
+    range = document.selection.createRange();
+    if (range && range.parentElement() == textAreaElement) {
+      length = textAreaElement.value.length;
+      normalizedValue = textAreaElement.value.replace(/\r\n/g, "\n");
+
+      // Create a working TextRange that lives only in the input
+      textInputRange = textAreaElement.createTextRange();
+      textInputRange.moveToBookmark(range.getBookmark());
+
+      // Check if the start and end of the selection are at the very end
+      // of the input, since moveStart/moveEnd don't return what we want
+      // in those cases
+      endRange = textAreaElement.createTextRange();
+      endRange.collapse(false);
+
+      if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+        start = end = len;
+      } else {
+        start = -textInputRange.moveStart("character", -length);
+        start += normalizedValue.slice(0, start).split("\n").length - 1;
+
+        if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+          end = length;
+        } else {
+          end = -textInputRange.moveEnd("character", -length);
+          end += normalizedValue.slice(0, end).split("\n").length - 1;
+        }
+      }
+    }
+  }
+
+  return {
+    start: start,
+    end: end
+  };
+}
+
+function insert_text(current_text,
+                     start_selection_position,
+                     end_selection_position,
+                     text_to_insert) {
+  before_cursor_text = current_text.substring(0, start_selection_position);
+  after_cursor_text = current_text.substring(end_selection_position, current_text.length);
+  modified_text = before_cursor_text + text_to_insert + after_cursor_text;
+  after_paste_cursor_position = (before_cursor_text + text_to_insert).length;
+  return [modified_text, after_paste_cursor_position];
+}
